a96792ca28840e79d45397af8450ae4bebbb125d
Andreas Thomas <dev@chronark.com>
2020-07-27 17:43:55 +0200

    Add test for opening the mobile navbar

--- libs/components/src/lib/navbar/navbar.test.tsx
+++ libs/components/src/lib/navbar/navbar.test.tsx
@@ -1,10 +1,35 @@
 import React from "react"
+import { cleanup, fireEvent, render, waitForElement } from '@testing-library/react';
 import renderer from "react-test-renderer"
 import Navbar from "./navbar"
+
+
+afterEach(cleanup)
 
 describe(" Navbar", () => {
   it("renders correctly", () => {
     const tree = renderer.create(<Navbar logo={<span>I am a logo</span>} />).toJSON()
     expect(tree).toMatchSnapshot()
   })
+
+
+
+  describe("on mobile", () => {
+    describe('when the user clicks on the hamburger icon', () => {
+      it("opens the navbar menu", async () => {
+        const { queryByTestId } = render(
+          <Navbar logo={<span>Logo</span>} />
+        );
+       
+        expect(queryByTestId("mobileNavbar")).toBeNull()
+
+        fireEvent.click(queryByTestId("toggle"))
+        expect(queryByTestId("mobileNavbar")).toBeTruthy()
+      })
+    })
+
+  })
+
+
+
 })

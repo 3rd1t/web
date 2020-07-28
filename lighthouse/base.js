b9f5a4b231924039f1db02f46a3cb82a2719e8d8
Andreas Thomas <dev@chronark.com>
2020-07-28 14:37:49 +0200

    Fix lighthouse tests

--- lighthouse/base.js
+++ lighthouse/base.js
@@ -1,15 +1,14 @@
 module.exports = {
-    ci: {
-      // assert: {
-      //   preset: "lighthouse:recommended",
-      // },
-      collect: {
-        numberOfRuns: 5,
-        url: ["http://localhost:4200/"],
-      },
-      upload: {
-        target: "temporary-public-storage",
-      },
+  ci: {
+    // assert: {
+    //   preset: "lighthouse:recommended",
+    // },
+    collect: {
+      numberOfRuns: 5,
+      url: ["http://localhost:4200/"],
     },
-  }
-
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+}
--- lighthouse/project..js
+++ lighthouse/project..js
@@ -1,7 +1,5 @@
 const base = require("./base")
 
 base.ci.collect.staticDistDir = "dist/apps/project/exported"
-base.ci.collect.url = [
-    "localhost:4200"
-]
-module.exports = base 
+base.ci.collect.url = ["http://localhost:4200/"]
+module.exports = base

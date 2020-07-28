9748d276aaa5f774538dd4b967492085c2e21e8d
Andreas Thomas <dev@chronark.com>
2020-07-28 09:50:40 +0200

    Change headline

--- apps/research-e2e/src/integration/index.test.ts
+++ apps/research-e2e/src/integration/index.test.ts
@@ -2,7 +2,7 @@ describe("research", () => {
   beforeEach(() => cy.visit("/"))
 
   it("should display header message", () => {
-    cy.contains("Get the right factor returns from our API")
+    cy.contains("Get the right factor returns from our research-friendly API")
   })
   describe("when a user clicks on the netlify badge", () => {
     it("should open the netlify page in a new tab", () => {})

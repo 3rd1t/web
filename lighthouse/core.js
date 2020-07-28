a99cf249919ee95f2402df045ac4d669fb424a23
Andreas Thomas <dev@chronark.com>
2020-07-28 12:01:07 +0200

    Refactor lhci config

--- lighthouse/core.js
+++
@@ -1,5 +0,0 @@
-const base = require("./base")
-
-base.ci.collect.staticDistDir = "dist/apps/core/exported"
-
-module.exports = base
--- lighthouse/research.js
+++
@@ -1,5 +0,0 @@
-const base = require("./base")
-
-base.ci.collect.staticDistDir = "dist/apps/research/exported"
-
-module.exports = base

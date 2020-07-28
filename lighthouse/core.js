c0a3479dfc7b7d08a124ee17ae617ec7f6f8a88a
Andreas Thomas <dev@chronark.com>
2020-07-28 11:31:02 +0200

    Refactor lighthouse config to extend from base

--- lighthouse/core.js
+++ lighthouse/core.js
@@ -1,15 +1,10 @@
+import base from "./base"
+
 module.exports = {
+  ...base,
   ci: {
-    // assert: {
-    //   preset: "lighthouse:recommended",
-    // },
     collect: {
-      numberOfRuns: 5,
-      url: ["http://localhost:4200/"],
       staticDistDir: "dist/apps/core/exported",
-    },
-    upload: {
-      target: "temporary-public-storage",
     },
   },
 }

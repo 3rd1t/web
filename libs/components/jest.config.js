025c609d8005f1b10fdb19a87614592278c3704f
Andreas Thomas <dev@chronark.com>
2020-07-30 16:18:35 +0200

    Update

--- libs/components/jest.config.js
+++ libs/components/jest.config.js
@@ -3,14 +3,14 @@ module.exports = {
   displayName: "components",
   preset: "../../jest.config.js",
   globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
+    "ts-jest": {
+      tsConfig: "<rootDir>/tsconfig.test.json",
     },
   },
   // transformIgnorePatterns: ["<rootDir>/node_modules/"],
   transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nrwl/react/plugins/jest",
+    "^.+\\.[tj]sx?$": "ts-jest",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "html"],
   coverageDirectory: "../../coverage/libs/components",

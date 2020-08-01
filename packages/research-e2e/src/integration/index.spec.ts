a28de4237037b6b7824284cda1241346ab63a2c5
Andreas Thomas <dev@chronark.com>
2020-08-01 22:30:50 +0200

    Add index test

--- packages/research-e2e/src/integration/index.spec.ts
+++ packages/research-e2e/src/integration/index.spec.ts
@@ -2,6 +2,6 @@ describe("project", () => {
   beforeEach(() => cy.visit("/"))
 
   it("should display welcome message", () => {
-    cy.contains("Portfolio analytics insights for everyone")
+    cy.contains("Get the right factor returns from our research-friendly API")
   })
 })

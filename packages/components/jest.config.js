cb8dcb80cbdb14084888808abdc041a7b9385612
Andreas Thomas <dev@chronark.com>
2020-08-02 19:39:08 +0200

    Add Line implementation

--- packages/components/jest.config.js
+++ packages/components/jest.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   name: "components",
   preset: "../../jest.config.js",
+  globals: {
+    "ts-jest": {
+      tsConfig: "<rootDir>/tsconfig.spec.json",
+    },
+  },
   transform: {
-    "^.+\\.[tj]sx?$": ["babel-jest", { cwd: __dirname, configFile: "./babel-jest.config.json" }],
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nrwl/react/plugins/jest",
+    "^.+\\.[tj]sx?$": "ts-jest",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "html"],
   coverageDirectory: "../../coverage/packages/components",

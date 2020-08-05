54d8ae0245164881aad1f7c2853cd1088f94e073
Andreas Thomas <dev@chronark.com>
2020-08-05 13:32:47 +0200

    Format

--- packages/research/pages/index.tsx
+++ packages/research/pages/index.tsx
@@ -11,24 +11,24 @@ interface IndexProps {
 }
 
 const Index = (props: IndexProps) => {
-
   const [codeIsFullscreen, setCodeIsFullscreen] = useState(false)
 
   return (
     <>
       <Section bg="bg-gray-100 " className="relative h-screen" id="index">
         <AnimateSharedLayout>
-
-          <motion.div layout className="flex flex-col items-center px-4 space-y-8 sm:pt-24 md:pt-28 lg:pt-32 xl:pt-40 xl:flex-row xl:space-y-0 xl:space-x-8">
-
+          <motion.div
+            layout
+            className="flex flex-col items-center px-4 space-y-8 sm:pt-24 md:pt-28 lg:pt-32 xl:pt-40 xl:flex-row xl:space-y-0 xl:space-x-8"
+          >
             <motion.div layout>
-
               <HeroSection
                 headline="Get the right factor returns from our research-friendly API"
                 paragraph={
                   <p className="flex flex-col xl:flex-row">
-                    We offer Kenneth French’s factor returns data via convenient REST API. No csv imports and data cleaning needed. Just go ahead with your analysis.
-              </p>
+                    We offer Kenneth French’s factor returns data via convenient REST API. No csv imports and data
+                    cleaning needed. Just go ahead with your analysis.
+                  </p>
                 }
                 primaryButton={{
                   label: "Get started",
@@ -42,18 +42,18 @@ const Index = (props: IndexProps) => {
                 }}
               ></HeroSection>
             </motion.div>
-            <motion.div
-              layout>
+            <motion.div layout>
               <CodeExamples code={props.codeExamples}></CodeExamples>
             </motion.div>
           </motion.div>
 
-
           <div className="absolute inset-x-0 bottom-0 flex justify-center mb-40">
             <ScrollTip href="#features" className="text-gray-100 bg-research-600"></ScrollTip>
           </div>
-
         </AnimateSharedLayout>
+      </Section>
+      <Section bg="bg-white">
+        <span>Placeholder</span>
       </Section>
     </>
   )

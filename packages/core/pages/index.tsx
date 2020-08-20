726bc935df0f6e5861ec5ec3990671cd5a5467fa
Andreas Thomas <dev@chronark.com>
2020-08-20 10:46:15 +0200

    Use shield icon for 'Privacy and Security'

--- packages/core/pages/index.tsx
+++ packages/core/pages/index.tsx
@@ -72,7 +72,7 @@ const features = [
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeWidth={2}
-          d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
+          d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
         />
       </svg>
     ),
@@ -147,7 +147,7 @@ const Index = (props: IndexProps) => {
         <ul className="flex flex-col flex-wrap mx-auto my-24 md:flex-row">
           {features.map((f, index) => {
             return (
-              <li key={index} className="p-3  md:w-1/2 xl:w-1/3">
+              <li key={index} className="p-3 md:w-1/2 xl:w-1/3">
                 <Feature icon={f.icon} title={f.title} description={f.description}></Feature>
               </li>
             )

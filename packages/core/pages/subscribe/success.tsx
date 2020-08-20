9a1ad3c7f4bec58f5a5931d20ec0c3dfc658e140
Andreas Thomas <dev@chronark.com>
2020-08-20 12:26:12 +0200

    Use larger icon

--- packages/core/pages/subscribe/success.tsx
+++ packages/core/pages/subscribe/success.tsx
@@ -7,13 +7,18 @@ export interface SuccessProps {}
 
 export const Success = (props: SuccessProps) => {
   return (
-    <Section className="flex items-center min-h-screen">
+    <Section className="flex items-center min-h-screen bg-gray-100">
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+        <h1 className="text-xl text-gray-900 md:text-2xl lg:text-3xl xl:text-4xl">
           Thank you for subscribing, we will be in touch shortly.
         </h1>
         <Link href="/">
-          <a className="mt-6 text-sm font-semibold text-research-600 focus:outline-none focus:underline">← Go back</a>
+          <button className="flex items-center mt-6 space-x-2 text-sm font-semibold hover:text-gray-900 text-research-600 focus:outline-none focus:underline">
+            <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-5 h-5 arrow-left">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+            </svg>
+            <span>Go back</span>
+          </button>
         </Link>
       </div>
     </Section>

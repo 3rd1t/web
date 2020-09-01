e47674c646c507956d922f6db0f8524598aa8efd
Andreas Thomas <dev@chronark.com>
2020-09-01 18:52:39 +0200

    Fix validation icon color

--- packages/urlbuilder/src/lib/builder/builder.tsx
+++ packages/urlbuilder/src/lib/builder/builder.tsx
@@ -79,12 +79,10 @@ export const Builder = (props: BuilderProps) => {
     },
   ]
 
-  const isValid = () => {
-    const v = fields.every((field) => {
+  const isValid = (): boolean => {
+    return fields.every((field) => {
       return field.status.state === State.DONE
     })
-    console.log(v)
-    return v
   }
 
   return (
@@ -112,16 +110,11 @@ export const Builder = (props: BuilderProps) => {
             </div>
           ))}
           <span className="p-2 font-thin bg-gray-100 border-l border-gray-300">
-            <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 text-green-500">
+            <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" className={`w-6 h-6 block ${isValid() ? "text-green-500" : "text-gray-700"}`}>
               {isValid() ? (
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
               ) : (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-                ></path>
+   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
               )}
             </svg>
           </span>

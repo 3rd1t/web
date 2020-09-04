27151c4697236b24b5b21af559c442a2a604919f
Andreas Thomas <dev@chronark.com>
2020-09-04 12:35:24 +0200

    Fix ts path /*

--- packages/research/pages/factors/three.tsx
+++ packages/research/pages/factors/three.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { MultiColumn } from "@perfolio/shared/nav"
 import { Builder } from "@perfolio/urlbuilder"
-import Content, { metadata, graph } from "../../../cms/src/research/factor/three.mdx"
+import Content, { metadata, graph } from "@perfolio/cms/research/factor/three.mdx"
 /* eslint-disable-next-line */
 export interface ThreeProps {}
 
@@ -11,17 +11,20 @@ export const Three = (props: ThreeProps) => {
       <MultiColumn breadcrumbs={["research", "factors", "three"]}>
         <div className="flex items-center justify-between p-16 mx-auto space-x-4 prose-sm md:prose lg:prose-lg max-w-none">
           <div className="w-3/5">
-            <div className="flex items-center space-x-4 text-sm">
+            <div className="flex items-center space-x-4 text-sm font-medium text-gray-600">
               <span>{metadata.authors}</span>
               <span>{metadata.publishYear}</span>
             </div>
             <div className="mt-2">
-              <Content />
+
+            <Content />
             </div>
           </div>
           <div className="w-2/5">{graph}</div>
         </div>
-        <div>{/* <Builder></Builder> */}</div>
+        <div>
+          {/* <Builder></Builder> */}
+        </div>
       </MultiColumn>
     </div>
   )

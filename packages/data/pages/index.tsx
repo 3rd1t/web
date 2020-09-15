918342883d49d45fd2c473766d33c1b889255a4d
Andreas Thomas <dev@chronark.com>
2020-09-15 19:59:24 +0200

    Add real feature sections

--- packages/data/pages/index.tsx
+++ packages/data/pages/index.tsx
@@ -10,12 +10,10 @@ import {
   FeatureSection,
   CTA,
 } from "@perfolio/shared/ui"
-import { motion, AnimateSharedLayout } from "framer-motion"
+import { FeatureList } from "../components/feature-list/feature-list"
 import fs from "fs"
-import Link from "next/link"
 import matter from "gray-matter"
 import Wrapper from "../components/wrapper/wrapper"
-import { Builders } from "../components/builders/builders"
 interface IndexProps {
   codeExamples: {
     language: string
@@ -28,100 +26,104 @@ const Index = (props: IndexProps) => {
   return (
     <Wrapper>
       <Section bg="bg-gray-100" className="min-h-screen" id="index">
-        <AnimateSharedLayout>
-          <motion.div
-            layout
-            className="flex flex-col items-center px-4 space-y-8 sm:pt-24 md:pt-28 lg:pt-32 xl:pt-40 lg:flex-row lg:space-y-0 lg:space-x-8"
-          >
-            <motion.div layout>
-              <HeroSection
-                headline="Get the right factor returns from our data-friendly API"
-                paragraph={
-                  <p className="flex flex-col xl:flex-row">
-                    We offer Kenneth French’s factor returns data via convenient REST API. No csv imports and data
-                    cleaning needed. Just go ahead with your analysis.
-                  </p>
-                }
-                primaryButton={{
-                  label: "Start now",
-                  href: "#features",
-                  className: "text-white bg-data-600 hover:bg-data-800",
-                }}
-                secondaryButton={{
-                  label: "Contact us",
-                  href: "mailto:info@perfol.io",
-                  className: "text-data-900 hover:text-data-700",
-                }}
-              ></HeroSection>
-            </motion.div>
-            <motion.div layout className="hidden lg:block">
-              <CodeExamples code={props.codeExamples}></CodeExamples>
-            </motion.div>
-          </motion.div>
-        </AnimateSharedLayout>
+        <div className="flex flex-col items-center px-4 space-y-8 sm:pt-24 md:pt-28 lg:pt-32 xl:pt-40 lg:flex-row lg:space-y-0 lg:space-x-8">
+          <div>
+            <HeroSection
+              headline="Make use of Perfolio's data power"
+              paragraph={
+                <p className="flex flex-col xl:flex-row">
+                  Take your analysis to the next level with financial data from our state-of-the-art scalable data
+                  platform.
+                </p>
+              }
+              primaryButton={{
+                label: "Start now",
+                href: "#features",
+                className: "text-white bg-data-600 hover:bg-data-800",
+              }}
+              secondaryButton={{
+                label: "Contact us",
+                href: "mailto:info@perfol.io",
+                className: "text-data-900 hover:text-data-700",
+              }}
+            ></HeroSection>
+          </div>
+          <div className="hidden lg:block">
+            <img src="https://via.placeholder.com/1280x720" alt="Placeholder for UI"></img>
+          </div>
+        </div>
       </Section>
       <Section id="data" bg="bg-white">
-        <div className="flex flex-col justify-center mt-20 space-y-4 lg:space-x-4 lg:space-y-0 lg:flex-row">
-          <FeatureSection
-            title="We offer great data"
-            subtitle="Sed laoreet eleifend nisl, eget ullamcorper velit gravida nec. Fusce porta diam tellus, sit. "
-            button={{ label: "Sign in", href: "/signin" }}
-            illustration={
-              <Wordcloud
-                words={[
-                  "Factor Returns",
-                  "Digitalization Ratings",
-                  "Risk Free Exchange",
-                  "Sustainability Ratings",
-                  "Currency Exchange",
-                ]}
-              />
-            }
-          />
-        </div>
+        <FeatureSection
+          title="High quality data, used in production by our own apps"
+          content={
+            <FeatureList
+              features={[
+                "Factor returns by Kenneth French in multiple currencies",
+                "Comprehensive data descriptions",
+                "Amazingly high data integrity and quality thanks to numerous in-depth tests",
+                "Soon available: Sustainability ratings, Digitalization ratings and many more!",
+              ]}
+            />
+          }
+          button={{ label: "Sign in", href: "/signin" }}
+          illustration={
+            <Wordcloud
+              words={[
+                "Factor Returns",
+                "Digitalization Ratings",
+                "Risk Free Exchange",
+                "Sustainability Ratings",
+                "Currency Exchange",
+              ]}
+            />
+          }
+        />
       </Section>
       <Section id="builders" bg="bg-gray-100">
-        <div className="flex flex-col justify-center mt-20 space-y-4 lg:space-x-4 lg:space-y-0 lg:flex-row">
-          <FeatureSection
-            rightAligned={true}
-            title="Build the data as you want"
-            subtitle="Sed laoreet eleifend nisl, eget ullamcorper velit gravida nec. Fusce porta diam tellus, sit. "
-            button={{ label: "Sign in", href: "/signin" }}
-            illustration={
-              <Wordcloud
-                words={[
-                  "Factor Returns",
-                  "Digitalization Ratings",
-                  "Risk Free Exchange",
-                  "Sustainability Ratings",
-                  "Currency Exchange",
-                ]}
-              />
-            }
-          />
-        </div>
+        <FeatureSection
+          rightAligned
+          title="Build datasets the way you want"
+          content={
+            <FeatureList
+              features={[
+                "Fetch data dynamically via our data-friendly API",
+                "Export data snapshots as json, csv or xlsx file",
+                "Get instant insights through our visualization tools",
+              ]}
+            />
+          }
+          button={{ label: "Sign in", href: "/signin" }}
+          illustration={<img src="https://via.placeholder.com/500x360" alt="Placeholder for UI"></img>}
+        />
       </Section>
       <Section id="charts" bg="bg-white">
-        <div className="flex flex-col justify-center mt-20 space-y-4 lg:space-x-4 lg:space-y-0 lg:flex-row">
-          <FeatureSection
-            title="Explore data with charts"
-            subtitle="Sed laoreet eleifend nisl, eget ullamcorper velit gravida nec. Fusce porta diam tellus, sit. "
-            button={{ label: "Sign in", href: "/signin" }}
-            illustration={
-              <Wordcloud
-                words={[
-                  "Factor Returns",
-                  "Digitalization Ratings",
-                  "Risk Free Exchange",
-                  "Sustainability Ratings",
-                  "Currency Exchange",
-                ]}
-              />
-            }
-          />
-        </div>
+        <FeatureSection
+          title="Create awesome charts quickly"
+          content={
+            <FeatureList
+              features={[
+                "From simple line charts to complex visualizations",
+                "Change the style according to your preferences",
+                "Export as image or svg file",
+              ]}
+            />
+          }
+          button={{ label: "Sign in", href: "/signin" }}
+          illustration={<img src="https://via.placeholder.com/500x360" alt="Placeholder for UI"></img>}
+        />
       </Section>
       <Section id="sources" bg="bg-gray-100">
+        <FeatureSection
+          rightAligned
+          title="Code"
+          content={
+            <FeatureList features={["Stop being at work", "And start working", "on perfolio,", "you're lazy!"]} />
+          }
+          illustration={<CodeExamples code={props.codeExamples}></CodeExamples>}
+        />
+      </Section>
+      <Section id="sources" bg="bg-white">
         <SectionTitle
           title="Sources"
           subtitle="We apply the latest finance data findings. Please give the authors and us some credit and cite correctly."
--- packages/shared/ui/src/lib/layout/feature-section/feature-section.tsx
+++ packages/shared/ui/src/lib/layout/feature-section/feature-section.tsx
@@ -1,39 +1,54 @@
 import React from "react"
 import Link from "next/link"
-
 export interface FeatureSectionProps {
   title: string
-  subtitle: string
-  button: { label: string; href: string }
+  content: React.ReactNode
+  button?: { label: string; href: string }
   illustration: React.ReactNode
   rightAligned?: boolean
 }
 
-export const FeatureSection = ({ title, subtitle, button, illustration, rightAligned }: FeatureSectionProps) => {
-  const link = (
+export const FeatureSection = ({ title, content, button, illustration, rightAligned }: FeatureSectionProps) => {
+  const link = button ? (
     <Link href={button.href}>
-      <a className="w-full px-8 py-3 font-medium text-gray-900 border border-gray-600 rounded-sm sm:max-w-xs md:py-4 md:text-lg md:px-10 focus:outline-none hover:bg-gray-100 hover:text-gray-700">
+      <a className="flex items-center font-medium text-data-700 sm:max-w-xs focus:outline-none hover:bg-gray-100 hover:text-gray-700">
         {button.label}
+        <svg
+          className="w-4 h-4 ml-2"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
+        </svg>
       </a>
     </Link>
-  )
+  ) : null
 
   return (
-    <div
-      className={`flex flex-col items-center justify-between  space-y-8 lg:space-y-0 lg:space-x-16 ${
-        rightAligned ? "lg:flex-row-reverse text-center lg:text-right" : "lg:flex-row lg:text-left"
-      }`}
-    >
-      <div className="w-full space-y-2 md:space-y-4 xl:space-y-6">
-        <h2 className="text-4xl font-extrabold leading-10 tracking-tight text-carbon-900 xl:text-5xl sm:leading-none md:text-5xl">
-          {title}
-        </h2>
-        <p className="text-carbon-600 sm:text-lg sm:mx-auto md:text-xl lg:mx-0">{subtitle}</p>
-        <div className="hidden lg:block">{link}</div>
+    <>
+      <h2
+        className={`text-4xl font-extrabold leading-10 tracking-tight text-gray-800 xl:text-5xl sm:leading-none md:text-5xl text-left ${
+          rightAligned ? "lg:text-right" : ""
+        }`}
+      >
+        {title}
+      </h2>
+      <div
+        className={`flex mt-16 items-center text-left justify-around space-y-8 lg:space-y-0 flex-col ${
+          rightAligned ? "lg:flex-row-reverse  lg:text-right" : "lg:text-left lg:flex-row"
+        }`}
+      >
+        <div className={`flex flex-grow ${rightAligned ? "justify-end" : "justify-start"}`}>{illustration}</div>
+        <div className="flex-grow">
+          <div className="space-y-8 text-left">
+            <div className="text-gray-700 sm:text-lg sm:mx-auto md:text-xl lg:mx-0">{content}</div>
+            <div className="text-left">{link}</div>
+          </div>
+        </div>
       </div>
-      <div>{illustration}</div>
-      <div className="lg:hidden">{link}</div>
-    </div>
+    </>
   )
 }
 

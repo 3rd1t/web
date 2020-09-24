cf7630cdeefecb19d9d2c56446430cf653c44ca0
Andreas Thomas <dev@chronark.com>
2020-09-24 12:38:34 +0200

    Add sidebar

--- packages/data/pages/builder/file.tsx
+++ packages/data/pages/builder/file.tsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react"
 import { MultiColumn } from "@perfolio/shared/nav"
 import { Select } from "@perfolio/shared/components"
+import { Transition } from "@tailwindui/react"
+import { finished } from "stream"
+
+enum State {
+  TODO,
+  ACTIVE,
+  DONE,
+}
 
 export interface FileProps {
   factorModels: { name: string; factors: { key: string; description: string }[] }[]
@@ -25,9 +33,57 @@ export const File = (props: FileProps) => {
       .then((json) => console.log(json))
   }
 
+  const steps = ["Model", "Factor", "Region", "Currency", "Interval"]
+  const [state, setState] = useState<State[]>(steps.map((_, i) => (i === 0 ? State.ACTIVE : State.TODO)))
+
+  const updateState = (i: number, newState: State) => {
+    const newStates = state.map((s) => s)
+    newStates[i] = newState
+    setState(newStates)
+  }
+
   return (
     <div>
       <MultiColumn breadcrumbs={["data", "builder", "file"]}>
+        <div className="flex items-center justify-center">
+          <aside className="flex flex-col space-y-4">
+            {steps.map((step, i) => (
+              <div className="flex items-center space-x-4">
+                <div
+                  className={`border-4 rounded-full ${
+                    state[i] === State.DONE
+                      ? "border-data-600 "
+                      : state[i] === State.ACTIVE
+                      ? "border-data-200"
+                      : "border-transparent"
+                  }`}
+                >
+                  <div
+                    className={`w-2 h-2 flex rounded-full ${
+                      state[i] === State.DONE || state[i] === State.ACTIVE ? "bg-data-600" : "bg-gray-300"
+                    }`}
+                  ></div>
+                </div>
+                <span
+                  className={`text-sm  leading-5 ${
+                    state[i] === State.ACTIVE ? "text-gray-900 font-semibold" : "text-gray-600 font-medium"
+                  } `}
+                >
+                  {step}
+                </span>
+              </div>
+            ))}
+          </aside>
+          <main>
+            <h2></h2>
+            <div></div>
+            <div>
+              <button>Prev</button>
+              <button>Next</button>
+            </div>
+          </main>
+        </div>
+
         <div className="max-w-lg p-20">
           <form>
             <Select

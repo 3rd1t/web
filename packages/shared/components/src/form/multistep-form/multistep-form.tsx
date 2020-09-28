eb184b415b0c1e3a0e9fa7f05a29ed11f21213b5
Andreas Thomas <dev@chronark.com>
2020-09-28 22:09:52 +0200

    Add mobile layout

--- packages/shared/components/src/form/multistep-form/multistep-form.tsx
+++ packages/shared/components/src/form/multistep-form/multistep-form.tsx
@@ -1,5 +1,4 @@
-import React, {  useState } from "react"
-import { Menu } from "@headlessui/react"
+import React, { useEffect, useState } from "react"
 import { Select } from "@perfolio/shared/components"
 import { AnimatePresence, AnimateSharedLayout, motion } from "framer-motion"
 
@@ -20,37 +19,61 @@ export const MultistepForm = (props: MultistepFormProps) => {
   const [progress, setProgress] = useState<number>(0)
   const [active, setActive] = useState<number>(0)
 
-  const updateState = (step: Step) => (v: number) => {
+  const updateState = (step: Step) => (newValue: number) => {
+    console.log({ progress, active })
     // Only advance if the field did not have a selection in the first place.
     if (step.value < 0) {
       setProgress(progress + 1)
     }
+    console.log("running")
     setActive(progress + 1)
-    step.setValue(v)
+    console.log("ran")
+
+    step.setValue(newValue)
   }
+
+  useEffect(() => {
+    console.log({ active })
+  }, [active])
+
+  useEffect(() => {
+    setActive(progress)
+  }, [progress])
+
   return (
     <div className="space-y-4">
-      {props.steps.map((step, i) => {
-        return (
-          <AnimatePresence>
-            {i <= progress ? (
-              <motion.div
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{
-                  damping: 300,
-                }}
-                onClick={() => {(active !== i) ?setActive(i) : setActive(-1)}}
-                className={`flex items-center justify-between p-3 border border-gray-400 hover:bg-gray-100 rounded ${
-                  i === progress ? "bg-gray-100" : ""
-                }`}
-              >
-                <AnimateSharedLayout>
-                  <motion.div layout className="flex flex-col items-start w-2/3">
-                    <div className="flex items-center space-x-3">
+      <AnimateSharedLayout>
+        {props.steps.map((step, i) => {
+          return (
+            <AnimatePresence>
+              {i <= progress ? (
+                <motion.div
+                  layout
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{
+                    damping: 300,
+                  }}
+                  onClick={() => {
+                    if (active !== i) {
+                      setActive(i)
+                    }
+                  }}
+                  className={`flex flex-col md:flex-row  md:items-center p-3 border md:justify-between border-gray-400 hover:bg-gray-100 rounded ${
+                    i === progress || i === active ? "bg-gray-100" : ""
+                  }`}
+                >
+                  <div className="md:w-2/3">
+                    <div className="flex items-center justify-start space-x-3">
                       <AnimatePresence>
                         {i < progress && i !== active ? (
-                          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+                          <motion.div
+                            layout
+                            initial={{ opacity: 0, x: -20, width: 0 }}
+                            animate={{ opacity: 1, x: 0, width: "auto" }}
+                            exit={{ opacity: 0, x:-20, width: 0 }}
+                            transition={{ damping: 300 }}
+                          >
                             <svg
                               className="w-6 h-6 text-data-600"
                               fill="currentColor"
@@ -74,26 +97,27 @@ export const MultistepForm = (props: MultistepFormProps) => {
                       {i === progress || i === active ? (
                         <motion.p
                           layout
-                          initial={{opacity:0, height:0, y:-10}}
-                          animate={{ opacity: 1, height: "auto", y:0 }}
-                          exit={{ opacity: 0, height: 0, y:-10 }}
+                          initial={{ opacity: 0, height: 0, y: -10 }}
+                          animate={{ opacity: 1, height: "auto", y: 0 }}
+                          exit={{ opacity: 0, height: 0, y: -10 }}
+                          transition={{ damping: 400 }}
                           className="text-sm text-gray-500 "
                         >
                           {step.description}
                         </motion.p>
                       ) : null}
                     </AnimatePresence>
-                  </motion.div>
-                </AnimateSharedLayout>
+                  </div>
 
-                <div className="w-1/3">
-                  <Select selected={step.value} setSelected={updateState(step)} choices={step.choices} />
-                </div>
-              </motion.div>
-            ) : null}
-          </AnimatePresence>
-        )
-      })}
+                  <div className="w-full mt-4 md:w-1/3 md:mt-0">
+                    <Select selected={step.value} setSelected={updateState(step)} choices={step.choices} />
+                  </div>
+                </motion.div>
+              ) : null}
+            </AnimatePresence>
+          )
+        })}
+      </AnimateSharedLayout>
     </div>
   )
 }
--- packages/shared/components/src/form/select/select.tsx
+++ packages/shared/components/src/form/select/select.tsx
@@ -1,73 +1,62 @@
-import React, { useState, useEffect } from "react"
-import { Transition } from "@tailwindui/react"
+import React, { useState } from "react"
+import { Transition, Menu } from "@headlessui/react"
 /* eslint-disable-next-line */
 export interface SelectProps {
   choices: string[]
   selected: number
   setSelected: React.Dispatch<React.SetStateAction<number>>
-  showLabel?: boolean
 }
 
 export const Select = ({ choices, selected, setSelected }: SelectProps) => {
-  const [dropdownOpen, setDropdownOpen] = useState(false)
-
   return (
-      <div className="relative">
-        <span className="inline-block w-full rounded-sm">
-          <button
-            onClick={() => setDropdownOpen(!dropdownOpen)}
-            type="button"
-            className="relative w-full py-2 pl-3 pr-10 text-left transition duration-150 ease-in-out bg-white border border-gray-300 rounded-sm cursor-default hover:border-data-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5"
-          >
-            <div className="flex items-center space-x-3">
+    <div className="relative w-full">
+      <Menu>
+        {({ open }) => (
+          <>
+            <Menu.Button className="w-full p-2 text-left transition duration-150 ease-in-out bg-white border border-gray-300 rounded-sm cursor-default hover:border-data-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
               <input
                 readOnly
-                onBlur={() => setDropdownOpen(false)}
-                className="block w-full truncate cursor-pointer focus:outline-none"
+                className="truncate cursor-pointer focus:outline-none"
                 value={selected >= 0 ? choices[selected] : ""}
               ></input>
-            </div>
-            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-              <svg className="w-5 h-5 text-gray-400" viewBox="0 0 20 20" fill="none" stroke="currentColor">
-                <path d="M7 7l3-3 3 3m0 6l-3 3-3-3" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-              </svg>
-            </span>
-          </button>
-        </span>
-
-        <Transition
-          show={dropdownOpen}
-          enter="transition-opacity duration-75"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition-opacity duration-150"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div
-            onMouseLeave={() => setDropdownOpen(false)}
-            className="absolute z-10 w-full mt-1 bg-white rounded shadow-lg"
-          >
-            <ul className="py-1 overflow-auto text-base leading-6 rounded shadow-xs max-h-56 focus:outline-none sm:text-sm sm:leading-5">
-              {choices.map((c, i) => (
-                <li
-                  onClick={() => {
-                    setSelected(i)
-                    setDropdownOpen(false)
-                  }}
-                  key={i}
-                  role="option"
-                  className="relative py-2 pl-3 text-gray-900 transition cursor-default select-none hover:bg-data-700 pr-9 hover:text-data-100"
-                >
-                  <div className="flex items-center space-x-3">
-                    <span className={`block font-normal truncate ${selected === i ? "font-semibold" : ""}`}>{c}</span>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </Transition>
-      </div>
+              <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                <svg className="w-5 h-5 text-gray-400" viewBox="0 0 20 20" fill="none" stroke="currentColor">
+                  <path d="M7 7l3-3 3 3m0 6l-3 3-3-3" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                </svg>
+              </span>
+            </Menu.Button>
+            <Transition
+              show={open}
+              enter="transition duration-100 ease-out"
+              enterFrom="transform scale-95 opacity-0"
+              enterTo="transform scale-100 opacity-100"
+              leave="transition duration-75 ease-out"
+              leaveFrom="transform scale-100 opacity-100"
+              leaveTo="transform scale-95 opacity-0"
+            >
+              <div className="absolute z-10 w-full mt-1 bg-white rounded-md shadow-lg">
+                <Menu.Items className="flex flex-col items-start py-1 overflow-auto text-base leading-6 rounded-sm shadow-xs max-h-56 focus:outline-none sm:text-sm sm:leading-5 ">
+                  {choices.map((choice, i) => {
+                    return (
+                      <Menu.Item className="w-full py-2 pl-3 text-left text-gray-900 cursor-default select-none hover:bg-data-300">
+                        {({ active }) => (
+                          <button
+                            onClick={() => setSelected(i)}
+                            className={`block font-normal truncate ${active ? "font-semibold" : ""}`}
+                          >
+                            {choice}
+                          </button>
+                        )}
+                      </Menu.Item>
+                    )
+                  })}
+                </Menu.Items>
+              </div>
+            </Transition>
+          </>
+        )}
+      </Menu>
+    </div>
   )
 }
 

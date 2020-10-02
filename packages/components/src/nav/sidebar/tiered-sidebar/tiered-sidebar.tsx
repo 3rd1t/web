b7fa2d6fe1100665bb511f27e41fd4a067d38299
Andreas Thomas <dev@chronark.com>
2020-10-02 12:36:21 +0200

    Remove unused imports (#417)

--- packages/components/src/nav/sidebar/tiered-sidebar/tiered-sidebar.tsx
+++ packages/components/src/nav/sidebar/tiered-sidebar/tiered-sidebar.tsx
@@ -1,10 +1,6 @@
-import { Transition } from "@headlessui/react"
 import React, { useState } from "react"
 import { Main } from "./main/main"
 import { Menu } from "./menu/menu"
-import Link from "next/link"
-import { Logo } from "@perfolio/shared/ui"
-import { AnimatePresence, motion } from "framer-motion"
 /* eslint-disable-next-line */
 export interface TieredSidebarProps {}
 
@@ -14,9 +10,6 @@ export const TieredSidebar = (props: TieredSidebarProps) => {
   const [open, setOpen] = useState(false)
   return (
     <div style={{ width: navbarWidth }}>
-      <button className="text-white" onClick={() => setOpen(!open)}>
-        Toggle
-      </button>
       <div className="relative">
         <div className="absolute border-r border-gray-300">
           <Main open={open} setOpen={setOpen} navbarWidth={navbarWidth}></Main>

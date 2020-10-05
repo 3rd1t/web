2ace1ff2f45378ac55c6beead373a944c4ef1bb5
Andreas Thomas <dev@chronark.com>
2020-10-05 14:55:13 +0200

    Remove old footer

--- packages/components/src/nav/footer/footer.tsx
+++ packages/components/src/nav/footer/footer.tsx
@@ -62,48 +62,6 @@ export const Footer = () => {
       </div>
     </footer>
   )
-
-  // return (
-  //   <footer className="">
-  //     <div className="container mx-auto">
-  //       <div className={`flex flex-wrap justify-between p-10  ${secondaryText}`}>
-  //         <div className="p-2 md:space-y-2">
-  //           <span className={`text-xs font-semibold ${primaryText} uppercase`}>Projects</span>
-  //           {footerLink("Home", "https://perfol.io")}
-  //           <div className="flex items-center space-x-2">
-  //             {footerLink("Analytics", "https://analytics.perfol.io")}
-  //             <span className={`px-1 text-xs ${secondaryText} rounded-sm border border-gray-100 font-semibold`}>
-  //               WIP
-  //             </span>
-  //           </div>
-  //           {footerLink("Data", "https://data.perfol.io")}
-  //         </div>
-  //         <div className="p-2 md:space-y-2">
-  //           <span className={`text-xs font-semibold ${primaryText} uppercase`}>Resources</span>
-  //           {footerLink("Documentation", "/docs")}
-  //           {footerLink("Roadmap", "https://perfol.io/roadmap")}
-  //           {footerLink("Imprint", "/imprint")}
-  //           {footerLink("Privacy", "/privacy")}
-  //         </div>
-  //         <div className="p-2 md:space-y-2">
-  //           <span className={`text-xs font-semibold ${primaryText} uppercase`}>Contact us</span>
-  //           {footerLink("contact@perfol.io", "mailto:info@perfol.io")}
-  //         </div>
-  //         <div className="p-2 md:space-y-2">
-  //           <span className={`text-xs font-semibold ${primaryText} uppercase`}>Source</span>
-  //           {footerLink("github.com", "https://github.com/perfolio")}
-  //         </div>
-  //       </div>
-  //     </div>
-  //     <div className={`pt-2 border-t ${borderColor}`}>
-  //       <div className="max-w-6xl mx-auto">
-  //         <div className="flex flex-col items-center px-10 py-2 text-sm md:flex-row">
-  //           <span className={`${primaryText}`}>© Copyright {new Date().getFullYear()}. All Rights Reserved.</span>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </footer>
-  // )
 }
 
 export default Footer

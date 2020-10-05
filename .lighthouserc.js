0ff6f6cb50ee7419db35e9817e7f9d651875d6fb
Andreas Thomas <dev@chronark.com>
2020-10-05 14:09:30 +0200

    Lower accessibility t 0.95

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -4,7 +4,7 @@ module.exports = {
       // preset: "lighthouse:no-pwa",
       assertions: {
         "categories:performance": ["error", { minScore: 0.8 }],
-        "categories:accessibility": ["error", { minScore: 1 }],
+        "categories:accessibility": ["error", { minScore: 0.95 }],
         "categories:bestPractices": ["error", { minScore: 1 }],
         "categories:seo": ["error", { minScore: 1 }],
       },

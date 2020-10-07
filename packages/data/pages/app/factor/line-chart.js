427897271c75f829c046ceeb4f37c63222a4c80c
madsjordt <mads.jordt@fau.de>
2020-10-07 16:39:01 +0200

    line chart visualization

--- packages/data/pages/app/factor/line-chart.js
+++ packages/data/pages/app/factor/line-chart.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Line} from 'react-chartjs-2'
+import React from 'react'
+import { Line } from 'react-chartjs-2'
 
 
 class LineChart extends React.Component {
@@ -98,9 +98,9 @@ class LineChart extends React.Component {
 
   render() {
     return (
-      <div className="chart">
+      <div className="LineChart">
         <Line
-          data={this.state.chartData}
+          data={this.chartData}
           width={1200}
           height={500}
           options={{

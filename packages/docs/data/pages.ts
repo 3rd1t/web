29d74ec7474746562239a99e00d038c5cfba25d6
madsjordt <56762020+madsjordt@users.noreply.github.com>
2020-10-29 16:41:35 +0100

    Dev 136/clean docs (#474)
    * Cleaned testing contents
    
    * Deleted testing content
    
    * Update pages.ts
    
    Co-authored-by: Andreas Thomas <dev@chronark.com>

--- packages/docs/data/pages.ts
+++ packages/docs/data/pages.ts
@@ -10,36 +10,6 @@ const data: Collection[] = [
       },
     ],
   },
-  {
-    name: "data",
-    documents: [
-      {
-        name: "data",
-        href: "/data/index",
-      },
-      {
-        name: "Secondpost",
-        href: "/data/secondpost",
-      },
-      {
-        name: "v2",
-        href: "/data/v2",
-      },
-    ],
-  },
-  {
-    name: "Something",
-    documents: [
-      {
-        name: "v2",
-        href: "/something/v2",
-      },
-      {
-        name: "Integration",
-        href: "/something/integration",
-      },
-    ],
-  },
 ]
 
 export default data

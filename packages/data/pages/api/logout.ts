e13ab59734c7cc1c7b4d412ef620efaaf835ecbb
Andreas Thomas <dev@chronark.com>
2020-10-30 18:51:04 +0100

    WIP This is a mess, but /app works on data

---
+++ packages/data/pages/api/logout.ts
@@ -0,0 +1,11 @@
+import {auth0} from '@perfolio/auth/auth0/auth0';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function logout(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  try {
+    await auth0.handleLogout(req, res);
+  } catch (error) {
+    console.error(error);
+    res.status(error.status || 400).end(error.message);
+  }
+}

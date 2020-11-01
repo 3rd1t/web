43064cc96f0c0b61cb92843e8c6164615ca59170
kevin-kohler <56868276+kevin-kohler@users.noreply.github.com>
2020-11-01 18:37:54 +0100

    Dev 76/page 404 docs (#457)
    * Add fetch UI mockup for data page and remove xlsx as type
    
    * Add new 404 page to docs
    
    * Add img and description for data mockup
    
    * Fix docs 404 page duplicate footer
    
    * Fix docs 404 page duplicate footer
    
    Co-authored-by: Andreas Thomas <dev@chronark.com>

---
+++ packages/docs/pages/404.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { NotFound404 as Content } from "@perfolio/pages"
+
+export const NotFound404 = () => {
+  return (
+      <Content />
+  )
+}
+
+export default NotFound404

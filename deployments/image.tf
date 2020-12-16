6bae9a960e55c0ac9aa2a79320bf1f55dd43dab6
Andreas Thomas <dev@chronark.com>
2020-12-16 22:34:31 +0100

    Add lifecycle

--- deployments/image.tf
+++ deployments/image.tf
@@ -3,12 +3,12 @@ data "docker_registry_image" "web_app" {
 }
 resource "docker_image" "web_app" {
   name          = data.docker_registry_image.web_app.name
-  pull_triggers = ["${data.docker_registry_image.web_app.sha256_digest}"]
+  pull_triggers = [data.docker_registry_image.web_app.sha256_digest]
 }
 data "docker_registry_image" "web_website" {
   name = "perfolio/web-website:${var.tag}"
 }
 resource "docker_image" "web_website" {
   name          = data.docker_registry_image.web_website.name
-  pull_triggers = ["${data.docker_registry_image.web_website.sha256_digest}"]
+  pull_triggers = [data.docker_registry_image.web_website.sha256_digest]
 }

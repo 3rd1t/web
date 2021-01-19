141e9193c3478ec03cdf1737fbaa4d7294c3586b
Andreas Thomas <dev@chronark.com>
2021-01-19 17:36:57 +0100

    load company when adding (#700)
    * Disable cache
    
    * Add prettier
    
    * Load company

--- apps/app/pages/add-transaction.tsx
+++ apps/app/pages/add-transaction.tsx
@@ -1,23 +1,91 @@
-import React, { useState } from "react";
-import { NextPageContext } from "next";
-import { SidebarLayout } from "@perfolio/components/nav/layout/sidebar-layout/sidebar-layout";
-import { requireUser } from "@perfolio/backend/api";
-import { User } from "@perfolio/backend/user";
-import { useRouter } from "next/router";
-import axios from "axios";
-import { Button } from "@perfolio/components/clickable/button/button";
+import React, { useEffect, useState } from "react"
+import { NextPageContext } from "next"
+import { SidebarLayout } from "@perfolio/components/nav/layout/sidebar-layout/sidebar-layout"
+import { requireUser } from "@perfolio/backend/api"
+import { User } from "@perfolio/backend/user"
+import { useRouter } from "next/router"
+import axios from "axios"
+import { Button } from "@perfolio/components/clickable/button/button"
+import { Transition } from "@headlessui/react"
+import { Icon } from "@perfolio/components/table/cells/icon/icon"
+import useSWR from "swr"
 
+const GET = async (url: string) => {
+  const res = await fetch(url)
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.")
+    // Attach extra info to the error object.
+    error.message = await res.json()
+    throw error
+  }
+  return res.json()
+}
+const Company = ({ isin }: { isin: string }) => {
+  const { data, error } = useSWR(`/api/get-company?isin=${isin}`, GET)
+  console.log(data)
+
+  if (error) {
+    return null
+  }
+  if (!data) {
+    return (
+      <svg
+        className="w-5 h-5 mr-3 -ml-1 text-white animate-spin"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+        ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    )
+  }
+  return (
+    <Icon
+      label={data.symbol.toUpperCase()}
+      content={data.name}
+      align="justify-start"
+      icon={
+        <img
+          src={`https://storage.googleapis.com/iex/api/logos/${data.symbol.toUpperCase()}.png`}
+          alt={`${data.symbol} logo`}
+          width="64"
+          height="64"
+        />
+      }
+    />
+  )
+}
+
+interface ICompany {
+  symbol: string
+  sector: string
+}
 const Question = ({ label }: { label: string }) => {
-  return <label className="text-lg font-medium text-gray-800">{label}</label>;
-};
+  return <label className="text-lg font-medium text-gray-800">{label}</label>
+}
 
 interface InputProps {
-  type: string;
-  label: string;
-  placeholder?: string;
-  value?: string | number;
-  disabled?: boolean;
-  onChange?: (e: React.FormEvent<HTMLInputElement>) => void;
+  type: string
+  label: string
+  placeholder?: string
+  value?: string | number
+  disabled?: boolean
+  onChange?: (e: React.FormEvent<HTMLInputElement>) => void
+  validate: (value: string | number) => boolean
 }
 const Input = ({
   type,
@@ -26,7 +94,9 @@ const Input = ({
   value,
   disabled,
   onChange,
+  validate,
 }: InputProps) => {
+  const isValid = validate(value)
   return (
     <div className="">
       <div className="space-y-1">
@@ -40,44 +110,59 @@ const Input = ({
             placeholder={placeholder}
             value={value}
             onChange={onChange}
-            className="flex items-center justify-between w-full px-6 pt-5 pb-2 space-x-8 text-lg text-gray-900 border border-gray-300 rounded-sm bg-gray-50 focus:bg-white ring-purple-700 focus:ring-1 focus:outline-none"
+            className={`flex items-center justify-between w-full px-6 pt-5 pb-2 space-x-8 text-lg text-gray-900  ${
+              isValid ? "border-gray-300 border" : "border-purple-300 border-2"
+            } rounded-sm bg-gray-50 focus:bg-white ring-purple-700 focus:ring-1 focus:outline-none`}
           />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 interface Transaction {
-  assetID: string;
-  quantity: number;
-  value: number;
-  executedAt: string;
+  assetID: string
+  quantity: number
+  value: number
+  executedAt: string
 }
 
 export interface AddTransactionProps {
-  user: User;
+  user: User
 }
 export const AddTransaction = ({ user }: AddTransactionProps) => {
-  const router = useRouter();
-  const [createAnother, setCreateAnother] = useState(false);
-
+  const router = useRouter()
+  const [createAnother, setCreateAnother] = useState(false)
+  const [company, setCompany] = useState<ICompany>(null)
   const [transaction, setTransaction] = useState<Transaction>({
     assetID: "",
     quantity: 1,
     value: 0,
     executedAt: new Date().toISOString().split(".")[0],
-  });
+  })
+  useEffect(() => {
+    const regex = new RegExp("([A-Z]{2}[a-zA-Z0-9]{10})")
+    const isValid = regex.test(transaction.assetID)
+    if (!isValid) {
+      setCompany(null)
+      return
+    }
+    fetch(`/api/get-company?isin=${transaction.assetID}`)
+      .then((res) => res.json())
+      .then((json) => {
+        setCompany(json)
+      })
+  }, [transaction.assetID])
 
   const updateTransaction = (key: string, value: number | string) => {
     setTransaction({
       ...transaction,
       [key]: value,
-    });
-  };
+    })
+  }
 
   const submit = async () => {
-    const url = "/api/add-transaction";
+    const url = "/api/add-transaction"
     await fetch(url, {
       method: "POST",
       body: JSON.stringify({
@@ -89,12 +174,12 @@ export const AddTransaction = ({ user }: AddTransactionProps) => {
         value: transaction.value,
         executedAt: Date.parse(transaction.executedAt) / 1000,
       }),
-    });
+    })
 
     if (!createAnother) {
-      router.push("/transactions");
+      router.push("/transactions")
     }
-  };
+  }
 
   return (
     <SidebarLayout
@@ -109,10 +194,7 @@ export const AddTransaction = ({ user }: AddTransactionProps) => {
         <div className="max-w-lg p-8 mx-auto bg-white border rounded">
           <h2 className="text-3xl font-medium">Add Transaction</h2>
 
-          <p className="text-gray-600 ">
-            Enter your transactions here, the price will be loaded
-            automatically.
-          </p>
+          <p className="text-gray-600 ">Enter your transactions here</p>
 
           <div className="mt-8 space-y-8">
             <div className="space-y-1">
@@ -155,7 +237,7 @@ export const AddTransaction = ({ user }: AddTransactionProps) => {
                         {t}
                       </span>
                     </button>
-                  );
+                  )
                 })}
               </div>
             </div>
@@ -165,18 +247,30 @@ export const AddTransaction = ({ user }: AddTransactionProps) => {
                   transaction.quantity > 0 ? "buy" : "sell"
                 }?`}
               />
-              <Input
-                type="text"
-                label="ISIN"
-                value={transaction.assetID}
-                onChange={(e) =>
-                  setTransaction({
-                    ...transaction,
-                    assetID: e.currentTarget.value,
-                  })
-                }
-              />
-              <div className="flex items-center pt-3 space-x-4">
+              <div className="flex items-center w-full space-x-4">
+                <div className={company == null ? "w-full" : "w-1/2"}>
+                  <Input
+                    type="text"
+                    label="ISIN"
+                    value={transaction.assetID}
+                    onChange={(e) =>
+                      setTransaction({
+                        ...transaction,
+                        assetID: e.currentTarget.value,
+                      })
+                    }
+                    validate={(value: string) =>
+                      RegExp("[A-Z]{2}[a-zA-Z0-9]{10}").test(value)
+                    }
+                  />
+                </div>
+                {company != null ? (
+                  <div className="w-1/2 border border-gray-300 rounded-sm bg-gray-50">
+                    <Company isin={transaction.assetID} />
+                  </div>
+                ) : null}
+              </div>
+              <div className="flex items-center justify-center pt-3 space-x-4">
                 <Input
                   type="number"
                   label="Shares"
@@ -187,6 +281,7 @@ export const AddTransaction = ({ user }: AddTransactionProps) => {
                       quantity: e.currentTarget.valueAsNumber,
                     })
                   }
+                  validate={(value: number) => value !== 0}
                 />
                 <Input
                   type="number"
@@ -198,6 +293,7 @@ export const AddTransaction = ({ user }: AddTransactionProps) => {
                       value: e.currentTarget.valueAsNumber,
                     })
                   }
+                  validate={(value: number) => value > 0}
                 />
               </div>
             </div>
@@ -215,6 +311,7 @@ export const AddTransaction = ({ user }: AddTransactionProps) => {
                   onChange={(e) =>
                     updateTransaction("executedAt", e.currentTarget.value)
                   }
+                  validate={(value: string) => value !== ""}
                 />
               </div>
             </div>
@@ -237,15 +334,15 @@ export const AddTransaction = ({ user }: AddTransactionProps) => {
         </div>
       </div>
     </SidebarLayout>
-  );
-};
+  )
+}
 
-export default AddTransaction;
+export default AddTransaction
 
 export async function getServerSideProps(ctx: NextPageContext) {
   return {
     props: {
       user: await requireUser(ctx),
     },
-  };
+  }
 }
--- apps/app/pages/api/add-transaction.ts
+++ apps/app/pages/api/add-transaction.ts
@@ -1,33 +1,29 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import axios from "axios";
-import { getAPI} from "@perfolio/backend/api";
-import { auth0} from "@perfolio/backend/auth0";
-import getConfig from "next/config";
+import { NextApiRequest, NextApiResponse } from "next"
+import axios from "axios"
+import { getAPI } from "@perfolio/backend/api"
+import { auth0 } from "@perfolio/backend/auth0"
+import getConfig from "next/config"
 
 export default auth0().requireAuthentication(
   async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-    if (req.method !== "POST"){
+    if (req.method !== "POST") {
       res.setHeader("Allow", ["POST"])
       res.status(405).end(`Method ${req.method} Not Allowed`)
       return
     }
-    const {apiAddr, token} = await getAPI(req, res)
+    const { apiAddr, token } = await getAPI(req, res)
 
     try {
       const payload = {
         token: token,
         ...JSON.parse(req.body),
       }
-      const apiResponse = await axios.post(
-        `${apiAddr}/v1/transaction`,
-        payload
-      );
+      const apiResponse = await axios.post(`${apiAddr}/v1/transaction`, payload)
       res.send(apiResponse.data)
-      res.end();
-
+      res.end()
     } catch (error) {
-      console.error(error);
-      res.status(error.status || 400).end(error.message);
+      console.error(error)
+      res.status(error.status || 400).end(error.message)
     }
-  }
-);
+  },
+)
